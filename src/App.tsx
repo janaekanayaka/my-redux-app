3dbe2a7567652f659e5c463f8f3fb6b7b0c222bc
janaka@codecommit-at-789734661575 <janaka@blackswan-technologies.com>
2024-09-18 21:29:37 +0530

    my first commit for react redux app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
+import Counter from './component/Counter';
 
-function App() {
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <h1>Redux Counter App</h1>
+        <Counter />
+      </div>
+    </Provider>
   );
-}
+};
 
 export default App;
---
+++ src/component/Counter.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { useAppDispatch, useAppSelector } from '../app/hooks';
+import { decrement, increment } from '../features/counter/counterSlice';
+
+
+const Counter: React.FC = () => {
+  const count = useAppSelector((state) => state.counter.value);
+  const dispatch = useAppDispatch();
+
+  return (
+    <div>
+      <h2>Counter: {count}</h2>
+      <button onClick={() => dispatch(increment())}>Increment</button>
+      <button onClick={() => dispatch(decrement())}>Decrement</button>
+    </div>
+  );
+};
+
+export default Counter;
